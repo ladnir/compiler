731e25d04437bb0f4b4b5a27318df85781dfde71
ladnir <rindalp@onid.orst.edu>
2014-01-30 15:39:58 -0800

    bad

---
+++ Compiler/parser/WhileLoopNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.parser
+{
+    class WhileLoopNode : Node , LocalScope
+    {
+        private ExpressionNode eval;
+        private LocalScope scope;
+
+        public WhileLoopNode(ExpressionNode eval, LocalScope scope)
+        {
+            // TODO: Complete member initialization
+            this.eval = eval;
+            this.scope = scope;
+        }
+
+        public bool inScope(Token name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addToScope(DeclarationNode localVar)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
